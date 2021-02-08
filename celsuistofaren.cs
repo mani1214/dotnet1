f14b534820852cef85ae390dc21c1a28f02c4993
mani1214 <78633513+mani1214@users.noreply.github.com>
2021-02-08 14:13:31 +0530

    Add files via upload

---
+++ celsuistofaren.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace celsiustofarehnheit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter celsius:");
+            double celsius = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine((celsius * 9 / 5) + 32);
+            Console.ReadLine();
+        }
+    }
+}
